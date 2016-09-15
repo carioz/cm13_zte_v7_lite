21042d855863c5454e97911aa9a7f54413041e92
HDC <hackerdoublecyber@gmail.com>
2016-09-15 08:06:33 +0800

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,7 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),ZTE_BLADE_V0720)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-endif
+include $(CLEAR_VARS)
+LOCAL_MODULE                  := asec_helper
+LOCAL_MODULE_TAGS             := optional
+LOCAL_SRC_FILES               := asec_helper.c
+LOCAL_SHARED_LIBRARIES        := \
+	libcutils \
+	liblog
+include $(BUILD_EXECUTABLE)
